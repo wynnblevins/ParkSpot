7be426b98280cdc83380dac5b69d17bfe03829a4
Karen Brant <karen.brant3@gmail.com>
2018-03-17 11:36:39 -0400

    Google Map Services getGoogle function

--- assets/scripts/google-maps-service.js
+++ assets/scripts/google-maps-service.js
@@ -1,1 +1,13 @@
-api_key=AIzaSyCXL4u3hi_MnkgjpFEfkJAC5XlJ25Tx-gE
+// api_key=AIzaSyCXL4u3hi_MnkgjpFEfkJAC5XlJ25Tx-gE
+function getGoogle() {
+    var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=3604+Riverchase+Court,+Richmond,VA&destination=University+of+Richmond&key=AIzaSyCXL4u3hi_MnkgjpFEfkJAC5XlJ25Tx-gE"
+    $.ajax( {
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) { 
+          var directions = response.data;
+          console.log(directions);
+});
+}
+
+getGoogle();
