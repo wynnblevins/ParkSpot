ad421df22786b70b8454211ae30187de24130aed
Robert (Wynn) Blevins <wynnblevins@yahoo.com>
2018-03-22 19:28:24 -0400

    Fixed some errors

--- assets/scripts/directionsPage/directions-page.js
+++ assets/scripts/directionsPage/directions-page.js
@@ -1,11 +1,18 @@
-(function (parksService) {
+(function (mapService) {
     'use strict';
     
     $(document).ready(function () {
+        console.log(window.location.href);
+        var reversedPath = window.location.href.split("").reverse().join(""); 
+        var firstSlashNdx = reversedPath.indexOf('/');
+        reversedDirections.splice(firstSlashNdx);
+        var path = reversedPath.split("").reverse().join("");
+        
         // pass a value for a radius around richmond to search for parks, in this case 10 miles
         mapService.getDirections(originText).then(function (response) {
             // putting directions in a variable called directions for the sake of readability
             var directions = response.routes[0].legs[0].steps;
+            console.log(directions);
         });
     });
-})(parksService);
+})(mapService);
