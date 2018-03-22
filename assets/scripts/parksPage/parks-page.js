d8dd0881cf1dd4a96005cd696ba41d6912d7fb08
Robert (Wynn) Blevins <wynnblevins@yahoo.com>
2018-03-22 18:48:06 -0400

    Pushing for will

--- assets/scripts/parksPage/parks-page.js
+++ assets/scripts/parksPage/parks-page.js
@@ -3,5 +3,10 @@
 
     $(document).ready(function () {
         console.log('Parks page is loaded.');
+
+        // ten mile radius
+        parksService.search(10).then(function (response) {
+            console.log(response);
+        });
     });
 })();
