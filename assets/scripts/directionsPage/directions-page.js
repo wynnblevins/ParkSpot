0f5a64af430378339b79c1eab7518be5e70bdcc3
Robert (Wynn) Blevins <wynnblevins@yahoo.com>
2018-03-23 14:05:39 -0400

    Directions to park, based on url params, are now being printed in console on page load.

--- assets/scripts/directionsPage/directions-page.js
+++ assets/scripts/directionsPage/directions-page.js
@@ -1,18 +1,21 @@
 (function (mapService) {
     'use strict';
-    
+
     $(document).ready(function () {
-        console.log(window.location.href);
-        var reversedPath = window.location.href.split("").reverse().join(""); 
-        var firstSlashNdx = reversedPath.indexOf('/');
-        reversedDirections.splice(firstSlashNdx);
-        var path = reversedPath.split("").reverse().join("");
+        var url_string = window.location.href; //window.location.href
+        var url = new URL(url_string);
+        var origin = url.searchParams.get("origin");
+        var destination = url.searchParams.get("destination");
         
-        // pass a value for a radius around richmond to search for parks, in this case 10 miles
-        mapService.getDirections(originText).then(function (response) {
-            // putting directions in a variable called directions for the sake of readability
-            var directions = response.routes[0].legs[0].steps;
-            console.log(directions);
-        });
+        function init() {
+            // pass a value for a radius around richmond to search for parks, in this case 10 miles
+            mapService.getDirections(origin, destination).then(function (response) {
+                // putting directions in a variable called directions for the sake of readability
+                var directions = response.routes[0].legs[0].steps;
+                console.log(directions);
+            });
+        }
+
+        init();
     });
 })(mapService);
--- assets/scripts/parksPage/parks-page.js
+++ assets/scripts/parksPage/parks-page.js
@@ -8,5 +8,21 @@
         parksService.search(10).then(function (response) {
             console.log(response);
         });
+
+        
+    });
+
+    $(document).on('click', 'a.btn', function myHandler() {
+        var $child = $(this).children();
+        $child.off();
+        var childText = $child.text();
+        
+        if (childText === 'clear') {
+            $child.text('done');
+        } else {
+            $child.text('clear');
+        }
+
+        $child.click(myHandler);
     });
 })();
