6a24ee52b45a51f88d0db7fa16359fc25e53bfb1
Karen Brant <karen.brant3@gmail.com>
2018-03-22 21:39:44 -0400

    Parks-pick.js

--- assets/scripts/parks-pick.js
+++ assets/scripts/parks-pick.js
@@ -1,21 +1,40 @@
 var url = new URL(window.location);
 
-// $(document).on('click', '.button.pic1', addDest(destination));
-// $(document).on('click', '.button.pic2', addDest(destination));
-// $(document).on('click', '.button.pic3', addDest(destination));
-// $(document).on('click', '.button.pic4', addDest(destination));
-// $(document).on('click', '.button.pic5', addDest(destination));
-// $(document).on('click', '.button.pic6', addDest(destination));
-// $(document).on('click', '.button.pic7', addDest(destination));
-// $(document).on('click', '.button.pic8', addDest(destination));
-// $(document).on('click', '.button.pic9', addDest(destination));
-// $(document).on('click', '.button.pic10', addDest(destination));
+$("#bryan-pic").click({
+
+});
+$(document).on('click', '.button.pic2', function (){
+
+});
+$(document).on('click', '.button.pic3', function (){
+
+});
+$(document).on('click', '.button.pic4', function (){
+
+});
+$(document).on('click', '.button.pic5', function (){
+
+});
+$(document).on('click', '.button.pic6', function (){
+
+});
+$(document).on('click', '.button.pic7', function (){
+
+});
+$(document).on('click', '.button.pic8', function (){
+
+});
+$(document).on('click', '.button.pic9', function (){
+
+});
+$(document).on('click', '.button.pic10', function () {
+
+});
 // console.log(url);
 // function addDest(address) {
 
 // }
-$("#testButton").click(function (){
-    // var destText = address; 
+$("#testButton").click(function (){ 
     var originText = mapService.getDirections(url.searchParams.get("origin")); 
     console.log(originText); 
     window.location.href = "./parks.html?origin=" + originText + "&destination=" + destText;  
